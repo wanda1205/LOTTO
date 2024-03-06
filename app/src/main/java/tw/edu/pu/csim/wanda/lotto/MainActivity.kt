671e8edad8854283741e2e308031be4583427526
wanda1205 <aa0925653552@gmail.com>
2024-03-06 17:45:43 +0800

    第三次commit (加入樂透功能 )

--- app/src/main/java/tw/edu/pu/csim/wanda/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/wanda/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.csim.wanda.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var t:TextView = findViewById(R.id.txv)
-        t.text = "app"
+        t.text = "0"
     }
+
+
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
+    }
+
 }
